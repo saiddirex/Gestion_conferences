384c6a309dff6b9441c0c466aa089f1deb24cd09
pierredupuis <pierre.dupuis1096@gmail.com>
2017-10-03 19:23:34 +0200

    Renamed files;Footer & Header PHP'ed;added .gitignore

---
+++ AjouterConf.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="icon" href="../../favicon.ico">
+
+	<title>Ajouter une conférence</title>
+
+	<!-- Bootstrap core CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/ajouterConf.css" rel="stylesheet">
+
+	<!-- Custom styles for this template -->
+	<link href="css/sticky-footer-navbar.css" rel="stylesheet">
+	</head>
+
+	<body>
+
+	<?php include("header.php"); ?>
+
+
+	<div class="container">
+		<div class="col-sm-10 offset-sm-1">
+			<div class="form-area">	 
+				<form role="form">
+					<br style="clear:both">
+					<h3 style="margin-bottom: 25px; text-align: center;">Contact Form</h3>
+					<div class="form-group">
+						<input type="text" class="form-control" id="name" name="name" placeholder="Name" required>
+					</div>
+					<div class="form-group">
+						<input type="text" class="form-control" id="email" name="email" placeholder="Email" required>
+					</div>
+					<div class="form-group">
+						<input type="text" class="form-control" id="mobile" name="mobile" placeholder="Mobile Number" required>
+					</div>
+					<div class="form-group">
+						<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" required>
+					</div>
+					<div class="form-group">
+					<textarea class="form-control" type="textarea" id="message" placeholder="Message" maxlength="140" rows="7"></textarea>
+						<span class="help-block"><p id="characterLeft" class="help-block ">You have reached the limit</p></span>					
+					</div>
+					
+					<button type="button" id="submit" name="submit" class="btn btn-primary pull-right">Submit Form</button>
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<?php include("footer.php"); ?>
+	
+	<!-- Bootstrap core JavaScript
+	================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+	<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="../../dist/js/bootstrap.min.js"></script>
+	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
+
+<!-- https://bootsnipp.com/snippets/featured/simple-contact-form -->
+
---
+++ home.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="icon" href="../../favicon.ico">
+
+	<title>Home</title>
+
+	<!-- Bootstrap core CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/home2.css" rel="stylesheet">
+
+	<!-- Custom styles for this template -->
+	<link href="css/sticky-footer-navbar.css" rel="stylesheet">
+	</head>
+
+	<body>
+
+	<!-- Fixed navbar -->
+	<?php include("header.php"); ?>
+
+	<div class="container">
+
+	  <!-- Page Heading -->
+		<h2 class="my-4"> Vendredi 10 Avril </h2>
+
+		
+		<div class="row">
+			<div class="col-md-3">
+				<p> 9:30 </p>
+			</div>
+			<div class="col-md-5">
+				<h3>Pause café inter-services</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
+				<a class="btn btn-primary" href="#">View Project</a>
+			</div>
+		</div>
+		
+		<hr>
+		
+		<div class="row">
+			<div class="col-md-3">
+				<p> 10:30 </p>
+			</div>
+			<div class="col-md-5">
+				<h3>Raclette ?</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
+				<a class="btn btn-primary" href="#">View Project</a>
+			</div>
+		</div>
+		
+		<hr>
+		
+		<br>
+		<h2 class="my-4"> Lundi 13 Avril </h2>
+		<div class="row">
+			<div class="col-md-3">
+				<p> 9:00 </p>
+			</div>
+			<div class="col-md-5">
+				<h3>Pétanque</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
+				<a class="btn btn-primary" href="#">View Project</a>
+			</div>
+		</div>
+		
+		<hr>
+		
+		<div class="row">
+			<div class="col-md-3">
+				<p> 12:45 </p>
+			</div>
+			<div class="col-md-5">
+				<h3>MacDo</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
+				<a class="btn btn-primary" href="#">View Project</a>
+			</div>
+		</div>
+	</div>
+	
+	<?php include("footer.php"); ?>
+
+
+	<!-- Bootstrap core JavaScript
+	================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+	<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="../../dist/js/bootstrap.min.js"></script>
+	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
