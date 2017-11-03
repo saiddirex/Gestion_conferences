5377db65a81b322d047ecb45595a5a823f4fcb98
said EL FARKH <saidelfarkh1990@gmail.com>
2017-11-03 21:09:23 +0100

    finalisation des pages html

--- Administration.php
+++ Administration.php
@@ -22,137 +22,96 @@
 	<!-- Fixed navbar -->
 	<?php include("header.php"); ?>
 	
+	<body>
 	<div class="container">
-	  <h2>Section title</h2>
-	  <div class="table-responsive">
-		<table class="table table-striped">
-		  <thead>
-			<tr>
-			  <th>#</th>
-			  <th>Header</th>
-			  <th>Header</th>
-			  <th>Header</th>
-			  <th>Header</th>
-			</tr>
-		  </thead>
-		  <tbody>
-			<tr>
-			  <td>1,001</td>
-			  <td>Lorem</td>
-			  <td>ipsum</td>
-			  <td>dolor</td>
-			  <td>sit</td>
-			</tr>
-			<tr>
-			  <td>1,002</td>
-			  <td>amet</td>
-			  <td>consectetur</td>
-			  <td>adipiscing</td>
-			  <td>elit</td>
-			</tr>
-			<tr>
-			  <td>1,003</td>
-			  <td>Integer</td>
-			  <td>nec</td>
-			  <td>odio</td>
-			  <td>Praesent</td>
-			</tr>
-			<tr>
-			  <td>1,003</td>
-			  <td>libero</td>
-			  <td>Sed</td>
-			  <td>cursus</td>
-			  <td>ante</td>
-			</tr>
-			<tr>
-			  <td>1,004</td>
-			  <td>dapibus</td>
-			  <td>diam</td>
-			  <td>Sed</td>
-			  <td>nisi</td>
-			</tr>
-			<tr>
-			  <td>1,005</td>
-			  <td>Nulla</td>
-			  <td>quis</td>
-			  <td>sem</td>
-			  <td>at</td>
-			</tr>
-			<tr>
-			  <td>1,006</td>
-			  <td>nibh</td>
-			  <td>elementum</td>
-			  <td>imperdiet</td>
-			  <td>Duis</td>
-			</tr>
-			<tr>
-			  <td>1,007</td>
-			  <td>sagittis</td>
-			  <td>ipsum</td>
-			  <td>Praesent</td>
-			  <td>mauris</td>
-			</tr>
-			<tr>
-			  <td>1,008</td>
-			  <td>Fusce</td>
-			  <td>nec</td>
-			  <td>tellus</td>
-			  <td>sed</td>
-			</tr>
-			<tr>
-			  <td>1,009</td>
-			  <td>augue</td>
-			  <td>semper</td>
-			  <td>porta</td>
-			  <td>Mauris</td>
-			</tr>
-			<tr>
-			  <td>1,010</td>
-			  <td>massa</td>
-			  <td>Vestibulum</td>
-			  <td>lacinia</td>
-			  <td>arcu</td>
-			</tr>
-			<tr>
-			  <td>1,011</td>
-			  <td>eget</td>
-			  <td>nulla</td>
-			  <td>Class</td>
-			  <td>aptent</td>
-			</tr>
-			<tr>
-			  <td>1,012</td>
-			  <td>taciti</td>
-			  <td>sociosqu</td>
-			  <td>ad</td>
-			  <td>litora</td>
-			</tr>
-			<tr>
-			  <td>1,013</td>
-			  <td>torquent</td>
-			  <td>per</td>
-			  <td>conubia</td>
-			  <td>nostra</td>
-			</tr>
-			<tr>
-			  <td>1,014</td>
-			  <td>per</td>
-			  <td>inceptos</td>
-			  <td>himenaeos</td>
-			  <td>Curabitur</td>
-			</tr>
-			<tr>
-			  <td>1,015</td>
-			  <td>sodales</td>
-			  <td>ligula</td>
-			  <td>in</td>
-			  <td>libero</td>
-			</tr>
-		  </tbody>
-		</table>
-	  </div>
+			<div class="row">
+					<div class="col-sm-6 col-sm-offset-2 text">
+							<h1><strong>ZZ_Agenda</strong> Administration Form</h1>
+					</div>
+					<div class="col-sm-2 col-sm-offset-10 ">
+							<button type="button" class="btn btn-success btn-sm" style="background-color:#33EB52"><span class="glyphicon glyphicon-plus"></span> Ajouter</button>
+
+					</div>
+			</div>
+
+			<table class="table">
+					<thead>
+							<tr>
+									<th style="text-align:left">Titre</th>
+									<th style="text-align:left">Nom Intervenant</th>
+									<th style="text-align:left">Localisation</th>
+									<th style="text-align:left">Date</th>
+									<th style="text-align:left">Action</th>
+							</tr>
+					</thead>
+					<tbody>
+							<tr>
+									<td style="text-align:left">Immergence du web</td>
+									<td style="text-align:left">said EL FARKH</td>
+									<td style="text-align:left">ISIMA</td>
+									<td style="text-align:left">30/09/2017</td>
+									<td style="text-align:left">
+											<button type="button" class="btn btn-success btn-sm"><span class="glyphicon glyphicon-pencil"></span> Modifier</button>
+											<button type="button" class="btn btn-danger  btn-sm"><span class="glyphicon glyphicon-remove"></span> Supprimer</button>
+									</td>
+
+							</tr>
+							<tr>
+									<td style="text-align:left">La mondialisation</td>
+									<td style="text-align:left">said EL FARKH</td>
+									<td style="text-align:left">ISIMA</td>
+									<td style="text-align:left">30/09/2017</td>
+									<td style="text-align:left">
+											<button type="button" class="btn btn-success btn-sm"><span class="glyphicon glyphicon-pencil"></span> Modifier</button>
+											<button type="button" class="btn btn-danger  btn-sm"><span class="glyphicon glyphicon-remove"></span> Supprimer</button>
+									</td>
+							</tr>
+							<tr>
+									<td style="text-align:left">La mondialisation</td>
+									<td style="text-align:left">said EL FARKH</td>
+									<td style="text-align:left">ISIMA</td>
+									<td style="text-align:left">30/09/2017</td>
+									<td style="text-align:left">
+											<button type="button" class="btn btn-success btn-sm"><span class="glyphicon glyphicon-pencil"></span> Modifier</button>
+											<button type="button" class="btn btn-danger  btn-sm"><span class="glyphicon glyphicon-remove"></span> Supprimer</button>
+									</td>
+							</tr>
+							<tr>
+									<td style="text-align:left">La mondialisation</td>
+									<td style="text-align:left">said EL FARKH</td>
+									<td style="text-align:left">ISIMA</td>
+									<td style="text-align:left">30/09/2017</td>
+									<td style="text-align:left">
+											<button type="button" class="btn btn-success btn-sm"><span class="glyphicon glyphicon-pencil"></span> Modifier</button>
+											<button type="button" class="btn btn-danger  btn-sm"><span class="glyphicon glyphicon-remove"></span> Supprimer</button>
+									</td>
+							</tr>
+							<tr>
+									<td style="text-align:left">La mondialisation</td>
+									<td style="text-align:left">said EL FARKH</td>
+									<td style="text-align:left">ISIMA</td>
+									<td style="text-align:left">30/09/2017</td>
+									<td style="text-align:left">
+											<button type="button" class="btn btn-success btn-sm"><span class="glyphicon glyphicon-pencil"></span> Modifier</button>
+											<button type="button" class="btn btn-danger  btn-sm"><span class="glyphicon glyphicon-remove"></span> Supprimer</button>
+									</td>
+							</tr>
+							<tr>
+									<td style="text-align:left">La mondialisation</td>
+									<td style="text-align:left">said EL FARKH</td>
+									<td style="text-align:left">ISIMA</td>
+									<td style="text-align:left">30/09/2017</td>
+									<td style="text-align:left">
+											<button type="button" class="btn btn-success btn-sm"><span class="glyphicon glyphicon-pencil"></span> Modifier</button>
+											<button type="button" class="btn btn-danger  btn-sm"><span class="glyphicon glyphicon-remove"></span> Supprimer</button>
+									</td>
+							</tr>
+					</tbody>
+			</table>
 	</div>
-	
+
+</body>
+
 	
 	<?php include("footer.php"); ?>
 
