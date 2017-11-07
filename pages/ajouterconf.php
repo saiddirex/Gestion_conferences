291025cb797adee503af86151aa6fb6f44875dc5
said EL FARKH <saidelfarkh1990@gmail.com>
2017-11-07 22:56:37 +0100

    MAJ des pages ajouterConf header et signin

--- pages/ajouterconf.php
+++ pages/ajouterconf.php
@@ -7,7 +7,7 @@
 	<meta name="author" content="">
 	<link rel="icon" href="../../favicon.ico">
 
-	<title>Home</title>
+	<title>AJOUT</title>
 
 	<!-- Bootstrap core CSS -->
 	<link href="../css/bootstrap.min.css" rel="stylesheet">
--- pages/header.php
+++ pages/header.php
@@ -2,7 +2,7 @@
 	<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
-		<a class="navbar-brand" href="#">Fixed navbar</a>
+		<a class="navbar-brand" href="../index.php"><span class="glyphicon glyphicon-th-large"></span><span style="color: #ED502E">  Z</span>Z_AGENDA</a>
 		<div class="collapse navbar-collapse" id="navbarCollapse">
 			<ul class="navbar-nav mr-auto">
 				<li class="nav-item">
@@ -11,16 +11,8 @@
 				<li class="nav-item">
 					<a class="nav-link" href="administration.php">Administration</a>
 				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="ajouterconf.php">Ajouter</a>
-				</li>
-				<div class="container">
-					<div class="row">
-						<div class="col-sm-2 col-sm-offset-8 ">
-						     <a class="btn btn-outline-success" href="signin.php">Sign In</a>
-						</div>
-					</div>
-			   </div>
+				
+				
             </ul>
 		</div>
 </nav>
