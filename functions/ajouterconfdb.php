618a0054c80a41f5ff8293d360a00707152ef4c0
pierredupuis <pierre.dupuis1096@gmail.com>
2017-11-10 16:15:23 +0100

    Securisation des variables POST

--- functions/ajouterconfdb.php
+++ functions/ajouterconfdb.php
@@ -7,21 +7,24 @@
 	{
 		return isset($str) && ! empty($str);
 	}
-
-
-	$_horaire = $_POST["horaire"];
-	$_titre = $_POST["titre"];
-	$_localisation = $_POST["lieu"];
-	$_intervenant = $_POST["intervenant"];
-	$_date = $_POST["date"];
-
+	function recupVar($varname)
+	{
+		return htmlspecialchars($_POST[$varname]);
+	}
+	
+	$_horaire = recupVar("horaire");
+	$_titre = recupVar("titre");
+	$_localisation = recupVar("lieu");
+	$_intervenant = recupVar("intervenant");
+	$_date = recupVar("date");
+	
 
 	if(checkArg($_horaire) && checkArg($_titre) && checkArg($_localisation) && checkArg($_intervenant) && checkArg($_date))
 	{
-		$xml=simplexml_load_file("../db/conference.xml");
+		$xml = simplexml_load_file("../db/conference.xml");
 
 		$newconf=$xml->addChild('conference');
-
+		
 		$newconf->addChild('horaire', $_horaire);
 		$newconf->addChild('titre', $_titre);
 		$newconf->addChild('localisation', $_localisation);
