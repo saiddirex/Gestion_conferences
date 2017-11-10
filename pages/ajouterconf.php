8f60b3e10c835f2fa8baaef8083851714da044ed
pierredupuis <pierre.dupuis1096@gmail.com>
2017-11-10 09:21:28 +0100

    Preparation page d'ajout de conference

--- pages/ajouterconf.php
+++ pages/ajouterconf.php
@@ -28,27 +28,30 @@
 	<div class="container">
 		<div class="col-sm-10 offset-sm-1">
 			<div class="form-area">	 
-				<form role="form">
+				<form role="form" action="functions/ajouterconfdb.php" method="post">
 					<br style="clear:both">
-					<h3 style="margin-bottom: 25px; text-align: center;">Contact Form</h3>
+					<h3 style="margin-bottom: 25px; text-align: center;">Ajouter une conférence</h3>
 					<div class="form-group">
-						<input type="text" class="form-control" id="name" name="name" placeholder="Name" required>
+						<label for="titre"> Titre </label>
+						<input type="text" class="form-control" id="titre" name="titre" required>
 					</div>
 					<div class="form-group">
-						<input type="text" class="form-control" id="email" name="email" placeholder="Email" required>
+						<label for="Lieu"> Lieu </label>
+						<input type="text" class="form-control" id="lieu" name="lieu" required>
 					</div>
 					<div class="form-group">
-						<input type="text" class="form-control" id="mobile" name="mobile" placeholder="Mobile Number" required>
+						<label for="intervenant"> Intervenant </label>
+						<input type="text" class="form-control" id="intervenant" name="intervenant" required>
 					</div>
 					<div class="form-group">
-						<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" required>
+						<label for="horaire"> Horaire </label>
+						<input type="text" class="form-control" id="horaire" name="horaire" required>
 					</div>
 					<div class="form-group">
-					<textarea class="form-control" type="textarea" id="message" placeholder="Message" maxlength="140" rows="7"></textarea>
-						<span class="help-block"><p id="characterLeft" class="help-block ">You have reached the limit</p></span>					
+						<label for="date"> Date </label>
+						<input type="text" class="form-control" id="date" name="date" required>
 					</div>
-					
-					<button type="button" id="submit" name="submit" class="btn btn-primary pull-right">Submit Form</button>
+					<button type="button" id="submit" name="submit" class="btn btn-primary pull-right">Créer la conférence</button>
 				</form>
 			</div>
 		</div>
