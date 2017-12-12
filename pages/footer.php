f7e02f218d6bc0a8a1c640364a9e3e31ee129c37
said EL FARKH <saidelfarkh1990@gmail.com>
2017-12-12 22:34:22 +0100

    ajout d'une portion de code pour definir le fichier a inclure en fonction de la langue choisie

--- pages/footer.php
+++ pages/footer.php
@@ -6,15 +6,15 @@
 			<div class="row">
 				<div class="col-md-4 f-about">
 					<a href="#">
-						<h1><span style="color: #ED502E">G</span>estion de conférence</h1>
+						<h1><span style="color: #ED502E"><?php echo $GestionDeConférence ?></h1>
 					</a>
-					<p>Plateforme et portail d'information des conférences animées à travers le monde.</p>
+					<p><?php echo $description ?></p>
 
 				</div>
 				<div class="col-md-2 l-posts">
 				</div>
 				<div class="col-md-6 f-contact">
-					<h3 class="widgetheading">Nous-contacter</h3>
+					<h3 class="widgetheading"><?php echo $Nous_contacter ?></h3>
 					<a href="mailto:gestion-conferences@gmail.com">
 						<p><i class="fa fa-envelope"></i> gestion-conference@gmail.com</p>
 					</a>
@@ -31,7 +31,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="copyright">
-					© 2016 Tout droit réservé | <a href="http://www.isima.fr">ISIMA</a>
+				<?php echo $copyright ?> | <a href="http://www.isima.fr">ISIMA</a>
 				</div>
 
 
